931ba40ad446722771df1b7f33574fddafb96f7b
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-18 16:48:55 +0300

    Create Color.py

---
+++ Color.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 15:13:10 2020
+
+@author: tatyanamironova
+
+data = input('What is your favourite color?')
+print('You said ' + data)
+"""
+
+
+print('What is your favourite color?')
+data = input()
+print('You said ' + data)
+
