72b6c528e3f4cea42ece496b2818a7045b629375
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-21 12:19:14 +0300

    Create 18:09:20_Logical operators.py

---
+++ 18:09:20_Logical operators.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 12:30:07 2020
+
+@author: tatyanamironova
+"""
+
+age = input('How old are you?')
+age = int(age)
+if age >2 and age <=8:
+    print('half price')
+else:
+    print('no discounts,sorry')
