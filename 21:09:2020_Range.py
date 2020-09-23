3913d42b02f276f00705be1039ae43f4e4897c1b
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-23 12:34:37 +0300

    Create 21:09:2020_Range.py

---
+++ 21:09:2020_Range.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 16:36:57 2020
+
+@author: tatyanamironova
+"""
+'''
+for num in range(10):
+    print(num)
+'''
+'''
+for num in range(0,20,2):
+    print(num)
+'''
+x = 0
+for n in range(10,21):
+    if n % 2 != 0:
+        x += n
+print(x)
+        
