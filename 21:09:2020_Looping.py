f5123167842baa1b60513615b11ab7b37b7a7859
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-23 12:34:03 +0300

    Create 21:09:2020_Looping.py

---
+++ 21:09:2020_Looping.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 16:07:51 2020
+
+@author: tatyanamironova
+"""
+'''
+for number in range(1,8):
+    print(number)
+    print(number*number)
+ '''   
+for letter in 'coffee':
+    print(letter)
