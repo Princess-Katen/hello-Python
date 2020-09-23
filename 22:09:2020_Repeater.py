1ef779c41090b47101fc0315eabcd2d2fd0df642
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-23 12:37:24 +0300

    Create 22:09:2020_Repeater.py

---
+++ 22:09:2020_Repeater.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 11:11:23 2020
+
+@author: tatyanamironova
+"""
+
+times = input('How many times do I have to tell you? ')
+times = int(times)
+
+for i in range(times):
+    print('Clean up your room')
+    print('iteration number', i)
+
