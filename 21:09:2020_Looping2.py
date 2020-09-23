d6c9fb47840d9c2ad134c94b17aed2c3b913db73
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-09-23 12:34:20 +0300

    Create 21:09:2020_Looping2.py

---
+++ 21:09:2020_Looping2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 16:26:47 2020
+
+@author: tatyanamironova
+"""
+'''
+r = range(10)
+print(list(r))
+'''
+nums = range(1,10,2)
+print(list(nums))
