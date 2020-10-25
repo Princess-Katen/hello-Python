0c3d2ad270a23e8791228b6b68e8329fd0abb358
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-25 14:14:59 +0300

    Create 25:10:2020_Colors_v.2.py

---
+++ 25:10:2020_Colors_v.2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 13:54:41 2020
+
+@author: tatyanamironova
+"""
+
+colors = ["purple", "teal", "orange"]
+the_best = colors[0]
+if "orange" in colors:
+    print('You have a great taste')
