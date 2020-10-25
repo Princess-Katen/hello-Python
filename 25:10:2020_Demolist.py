34a2e669fb1ae70247246f3ab5410bb11e3f1cd4
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-25 13:59:00 +0300

    Create 25:10:2020_Demolist.py
    Starting the Lists

---
+++ 25:10:2020_Demolist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 13:23:46 2020
+
+@author: tatyanamironova
+"""
+'''
+demo_list = ["a", 1,45, 6,77, True]
+len(demo_list)
+'''
+
+r = range(1,10)
