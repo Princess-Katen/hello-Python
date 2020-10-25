245956c0075b4fe8e088fad491894c8a3def1c45
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-25 13:59:16 +0300

    Create 25:10:2020_Colors.py

---
+++ 25:10:2020_Colors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 13:54:41 2020
+
+@author: tatyanamironova
+"""
+
+colors = ["purple", "teal", "orange"]
+the_best = colors[0]
