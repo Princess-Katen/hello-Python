71d97d58a129b5dd3862bcaca249bb4e8faf9e53
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-27 16:10:28 +0300

    Create 26:10:2020_For_Loop.py

---
+++ 26:10:2020_For_Loop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 13:54:41 2020
+
+@author: tatyanamironova
+"""
+"""
+colors = ["purple", "teal", "magenta", 8,9, True]
+for color in colors:    
+    print(color)
+    """
+    
+numbers = [4,5,6]
+for num in numbers:
+    print(num*num)
+    
