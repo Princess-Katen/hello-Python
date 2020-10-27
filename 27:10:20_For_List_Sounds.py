ab11587225946f05bf8a28453cc17fcfcd56ef3c
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-27 16:34:37 +0300

    Create 27:10:20_For_List_Sounds.py

---
+++ 27:10:20_For_List_Sounds.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:29:27 2020
+
+@author: tatyanamironova
+"""
+
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+result = " "
+for s in sounds:
+    result += s.upper()
+print(result)
