ace0ca0f2f601c2631130359da4b8dabf711da4b
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-27 17:11:07 +0300

    Create 27:10:20_List_Methods.py

---
+++ 27:10:20_List_Methods.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:34:29 2020
+
+@author: tatyanamironova
+"""
+"""
+data = [1,2,3]
+data.append("purple")
+print(data)
+"""
+"""
+correct_list = [1, 2, 3, 4]
+correct_list.extend([5, 6, 7, 8])
+print(correct_list)
+"""
+"""
+correct_list = [1, 2, 3, 4]
+correct_list.insert(2,"Katen!")
+print(correct_list)
+"""
+"""
+correct_list = [1, 2, 3, 4]
+correct_list.insert(-2, "Princess")
+print(correct_list)
+"""
+correct_list = [1, 2, 3, 4]
+correct_list.insert(len(correct_list), "Chudo")
+print(correct_list)
