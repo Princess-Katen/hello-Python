b9b3fbcd43ca67e161554adc884112c270392bc9
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-27 16:31:35 +0300

    Create 27:10:20_While_Loop.py

---
+++ 27:10:20_While_Loop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:08:59 2020
+
+@author: tatyanamironova
+"""
+
+colors = ["purple", "teal", "magenta", "blue", "white"]
+index = 0
+while index < len(colors):
+    print(f"{index}: {colors[index]}")
+    index += 1
+    
+    
+"""
+    print(colors[index])
+    index += 1
+"""
