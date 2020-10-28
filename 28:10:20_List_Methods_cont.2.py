7d79cb8d4483b0e1fccd2c697dbac325168d8a28
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-28 16:43:43 +0300

    Create 28:10:20_List_Methods_cont.2.py

---
+++ 28:10:20_List_Methods_cont.2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 16:16:21 2020
+
+@author: tatyanamironova
+"""
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+print(names.index("Bjorn"))
+print(names.index("Bjorn", 1))
+"""
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+print(names.count("Bjorn"))
+"""
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+names.reverse()
+print(names)
+"""
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+names.sort()
+print(names)
+names.append("Medved")
+print(names)
+names.sort()
+print(names)
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+lovers = ",".join(names)
+print(lovers)
