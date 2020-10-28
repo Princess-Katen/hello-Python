437d41b4a405f8a72a3722c61f59c9e12f4c8d1f
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-28 16:16:28 +0300

    Create 28:10:20_List_Methods_сont.py

---
+++ 28:10:20_List_Methods_сont.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:34:29 2020
+
+@author: tatyanamironova
+"""
+"""
+items = ["baking soda", "cacao", "coconut flour", "eggs"]
+items.pop()
+print(f"{items[-1]}")
+"""
+"""
+items = ["baking soda", "cacao", "coconut flour", "eggs"]
+items.pop()
+last_item = items.pop()
+print(items)
+"""
+"""
+items = ["baking soda", "cacao", "coconut flour", "eggs"]
+items.pop(1)
+print(items)
+"""
+"""
+items = ["baking soda", "cacao", "coconut flour", "eggs"]
+first = items.pop(0)
+print(first)
+"""
+names = ["Bjorn", "Bern", "Njedved", "Bear", "Bjorn"]
+names.remove("Bern")
+print(names)
+names.remove("Bjorn")
+print(names)
+names.remove("Jack")
+print(names)
