ff0bc569b3390c09ef8e7b18f6c19e70fba4cde0
Princess-Katen <70971743+Princess-Katen@users.noreply.github.com>
2020-10-29 17:40:16 +0300

    Create 29:10:20_List_Comprehensions.py

---
+++ 29:10:20_List_Comprehensions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 16:50:45 2020
+
+@author: tatyanamironova
+"""
+"""
+nums = [1,2,3]
+print([x*10 for x in nums])
+print([x/2 for x in nums])
+"""
+"""
+name = "bjorn"
+print([char.upper() for char in name])
+"""
+"""
+print([num*10 for num in range(1,6)])
+print([bool(val) for val in [0, [], "", ]])
+"""
+"""
+candles = ["caramel", "gingerbread", "cotton", "vanilla"]
+print([candle.upper() for candle in candles])
+"""
+nums = [1,2,3]
+print([str(num) + "Katten" for num in nums])
